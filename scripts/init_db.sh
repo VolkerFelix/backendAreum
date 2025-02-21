6f158a911ecf8ac43180e16e1c7210fb418ed407
Volker Leukhardt <volker.leukhardt@gmail.com>
2025-02-21 12:51:36 +0000

    Add checks for psql and sqlx

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed."
+    exit 1
+fi
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "Use:"
+    echo >&2 " cargo install --version='~0.8' sqlx-cli \
+--no-default-features --features rustls,postgres"
+    echo >&2 "to install it."
+    exit 1
+fi
+
 # Load .env file
 PARENT_DIR="$(dirname "$(pwd)")"
 if [ -f "$PARENT_DIR/.env" ]; then
