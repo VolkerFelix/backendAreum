14e0b1e8084ddf317be16cba26aa66b109b0e433
Volker Leukhardt <volker.leukhardt@gmail.com>
2025-02-21 12:12:55 +0000

    init sb script working

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
-set -x
 set -eo pipefail
 
-echo ${POSTGRES_USER}
-echo ${POSTGRES_PASSWORD}
-echo ${POSTGRES_DB}
-echo ${POSTGRES_PORT}
+# Load .env file
+PARENT_DIR="$(dirname "$(pwd)")"
+if [ -f "$PARENT_DIR/.env" ]; then
+    export $(grep -v '^#' "$PARENT_DIR/.env" | xargs)
+else
+    echo ".env file not found in the parent directory."
+    exit 1
+fi
+
 # Launch postgres using Docker
 docker run \
 -e POSTGRES_USER=${POSTGRES_USER} \
 -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
 -e POSTGRES_DB=${POSTGRES_DB} \
--p POSTGRES_PORT:${POSTGRES_PORT} \
+-p ${POSTGRES_PORT}:${POSTGRES_PORT} \
 -d postgres \
 postgres -N 1000
 # ^ Increased maximum number of connections for testing purposes
