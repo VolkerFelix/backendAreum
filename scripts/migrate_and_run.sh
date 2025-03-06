46ea85bbf18f44fcb714619c87a8f44fb9cd465c
Volker Leukhardt <volker.leukhardt@gmail.com>
2025-03-06 13:52:37 +0000

    set DATABASE_URL in run script

--- scripts/migrate_and_run.sh
+++ scripts/migrate_and_run.sh
@@ -3,7 +3,11 @@ set -e
 
 if [ "$APP_ENVIRONMENT" = "production" ]; then
   echo "Production environment detected. Running database migrations..."
-  sqlx migrate run
+  if [ -n "$DATABASE_URL" ]; then
+    DATABASE_URL="$DATABASE_URL" sqlx migrate run
+  else
+    echo "ERROR: DATABASE_URL is not set. Migrations cannot be run."
+  fi
 else
   echo "Non-production environment. Skipping automatic migrations."
 fi
